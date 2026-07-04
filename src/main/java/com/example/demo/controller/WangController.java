9840d604950378cc8cefb3e9d5b926b566edfe5b
wangwanbo <893433055@qq.com>
2026-07-04 22:41:25 +0800

    初始化

---
+++ src/main/java/com/example/demo/controller/WangController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/wang")
+@Tag(name = "测试类")
+public class WangController {
+
+    @Operation(summary = "测试聊天")
+    @RequestMapping(value = "/chat")
+    private String chat(String msg) {
+        System.out.printf("msg:" + msg);
+        return "OK";
+    }
+
+
+}
