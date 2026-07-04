b20de482875a7ecb682b921b2bd6f1df6f705ea2
wangwanbo <893433055@qq.com>
2026-07-04 22:42:20 +0800

    优化路径

--- src/main/java/com/wang/demo/controller/WangController.java
+++ src/main/java/com/wang/demo/controller/WangController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.wang.demo.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
--- src/test/java/com/wang/demo/DemoApplicationTests.java
+++ src/test/java/com/wang/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.wang.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
