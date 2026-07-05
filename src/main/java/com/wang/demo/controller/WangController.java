7b37ec4786c954b312ff5713b06be5fd7ed9a468
wangwanbo <893433055@qq.com>
2026-07-05 08:28:07 +0800

    优化代码

--- src/main/java/com/wang/demo/controller/WangController.java
+++ src/main/java/com/wang/demo/controller/WangController.java
@@ -2,6 +2,7 @@ package com.wang.demo.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WangController {
 
     @Operation(summary = "测试聊天")
-    @RequestMapping(value = "/chat")
+    @GetMapping(value = "/chat")
     private String chat(String msg) {
         System.out.printf("msg:" + msg);
         return "OK";
